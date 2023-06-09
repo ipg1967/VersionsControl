93833e851a4a37eda32f2efffac45de6720ecf29
ipg <>
2023-06-09 20:45:09 +0300

    yjdsq rjl

---
+++ codingC/Program.cs
@@ -0,0 +1,5 @@
+﻿
+Console.WriteLine("введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = number * number;
+System.Console.WriteLine("квадрат числа равен :" + result);
